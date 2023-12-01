3cd2d5d3be880496520642204167dc56099fc0a4
Igor Ghiberti <igorgh@outlook.com>
2023-12-01 11:56:54 -0300

    Commit inicial

---
+++ ProcessoSeletivoAcessBrasil/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ProcessoSeletivoAcessBrasil
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] amostra = { 100, 90, 40, 70 };
+            int alunoAcimaMedia = 0;
+            int mediaAmostra = (amostra.Sum()) / 4;
+
+            for (int indice=0; indice<amostra.Length; indice++)
+            {
+                if (amostra[indice] > mediaAmostra)
+                {
+                    alunoAcimaMedia++;
+                }
+            }
+
+            double eficiencia = (alunoAcimaMedia * 100 / amostra.Length);
+
+            Console.WriteLine($"A eficiência de alunos equivale a {eficiencia}%");
+            
+        }
+    }
+}
